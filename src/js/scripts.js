d4b674ff5e4014df9c5caf7d369f15b4d3bebfd7
vramirez2022 <veronica.ramirez2022@outlook.com>
2023-06-03 19:41:23 -0400

    changed CSS styles & change questionBox script

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,91 +1,103 @@
-import data from "./../dataset/dataset.json";
+import data from './../dataset/dataset.json';
 
 const musicData = data.music;
-const modArt = data["modern-art"];
+const modArt = data['modern-art'];
 const codingData = data.coding;
 
 // Add Event listener for Music Button click
-var musicBtn = document.getElementById("musicButton");
-musicBtn.addEventListener("click", () => {
+var musicBtn = document.getElementById('musicButton');
+musicBtn.addEventListener('click', () => {
   loadQuiz(musicData[0]);
 });
 
 // Add Event listener for Modern Art click
-var modernArtBtn = document.getElementById("modernArtButton");
-modernArtBtn.addEventListener("click", () => {
+var modernArtBtn = document.getElementById('modernArtButton');
+modernArtBtn.addEventListener('click', () => {
   loadQuiz(modArt[0]);
 });
 
 // Add Event listener for coding click
-var codingBtn = document.getElementById("codingButton");
-codingBtn.addEventListener("click", () => {
+var codingBtn = document.getElementById('codingButton');
+codingBtn.addEventListener('click', () => {
   loadQuiz(codingData[0]);
 });
 
 function loadQuiz(data) {
-  console.log(data);
-
   //get parent Div
-  var parentNode = document.getElementById("mainDiv");
-  var topicBox = document.getElementById("topicDiv");
+  var parentNode = document.getElementById('mainDiv');
+  var topicBox = document.getElementById('topicDiv');
   topicBox.remove();
 
   // create Question Box --> it will contain question details
-  var questionBox = document.createElement("div");
-  questionBox.setAttribute("id", "question-box");
+  var questionBox = document.createElement('div');
+  questionBox.setAttribute('id', 'question-box');
+  var titleElement = document.createElement('div');
+  titleElement.setAttribute('id', 'title');
+  titleElement.textContent = 'Music-Quiz Pop';
+  var scoreElement = document.createElement('p');
+  scoreElement.setAttribute('id', 'score');
+  scoreElement.textContent = 'Score: 0/10';
+  var closeButton = document.createElement('button');
+  closeButton.setAttribute('id', 'close-btn');
+  closeButton.textContent = 'Close Quiz';
+  var questionHeader = document.createElement('h2');
+  questionHeader.setAttribute('id', 'questionHeader');
+  questionHeader.innerHTML = 'Question 1/10: Question Description';
 
   // create Question paragraph
-  var questionPara = document.createElement("p");
+  var questionPara = document.createElement('p');
   questionPara.innerHTML = data.question;
 
   //create first  Option
-  var firstOption = document.createElement("input");
-  firstOption.setAttribute("type", "radio");
-  firstOption.setAttribute("name", "radioButton");
+  var firstOption = document.createElement('input');
+  firstOption.setAttribute('type', 'radio');
+  firstOption.setAttribute('name', 'radioButton');
 
   //set label for first option
-  var firstLabel = document.createElement("Label");
+  var firstLabel = document.createElement('Label');
   firstLabel.innerHTML = data.options[0];
-  var break1 = document.createElement("br");
+  var break1 = document.createElement('br');
 
   //create second  Option
-  var secondOption = document.createElement("input");
-  secondOption.setAttribute("type", "radio");
-  secondOption.setAttribute("name", "radioButton");
+  var secondOption = document.createElement('input');
+  secondOption.setAttribute('type', 'radio');
+  secondOption.setAttribute('name', 'radioButton');
 
   //set label for second option
-  var secondLabel = document.createElement("Label");
+  var secondLabel = document.createElement('Label');
   secondLabel.innerHTML = data.options[1];
-  var break2 = document.createElement("br");
+  var break2 = document.createElement('br');
 
   //create third  Option
-  var thirdOption = document.createElement("input");
-  thirdOption.setAttribute("type", "radio");
-  thirdOption.setAttribute("name", "radioButton");
+  var thirdOption = document.createElement('input');
+  thirdOption.setAttribute('type', 'radio');
+  thirdOption.setAttribute('name', 'radioButton');
 
   //set label for third option
-  var thirdLabel = document.createElement("Label");
+  var thirdLabel = document.createElement('Label');
   thirdLabel.innerHTML = data.options[2];
-  var break3 = document.createElement("br");
+  var break3 = document.createElement('br');
 
   //create fourth  Option
-  var fourthOption = document.createElement("input");
-  fourthOption.setAttribute("type", "radio");
-  fourthOption.setAttribute("name", "radioButton");
+  var fourthOption = document.createElement('input');
+  fourthOption.setAttribute('type', 'radio');
+  fourthOption.setAttribute('name', 'radioButton');
 
   //set label for fourth option
-  var fourthLabel = document.createElement("Label");
+  var fourthLabel = document.createElement('Label');
   fourthLabel.innerHTML = data.options[3];
-  var break4 = document.createElement("br");
+  var break4 = document.createElement('br');
 
   // Create a button for Previous Question
-  var prevButton = document.createElement("button");
-  var text = document.createTextNode("Previous");
+  var prevButton = document.createElement('button');
+  prevButton.setAttribute('id', 'previousBtn');
+  var text = document.createTextNode('Previous');
   prevButton.appendChild(text);
 
   // Create a button for Next Question
-  var nextButton = document.createElement("button");
-  var text = document.createTextNode("Next");
+  var nextButton = document.createElement('button');
+  var text = document.createTextNode('Next');
+  nextButton.setAttribute('id', 'nextBtn');
   nextButton.appendChild(text);
 
   questionBox.appendChild(questionPara);
@@ -109,4 +121,8 @@ function loadQuiz(data) {
   questionBox.appendChild(prevButton);
   questionBox.appendChild(nextButton);
   parentNode.appendChild(questionBox);
+  questionBox.appendChild(titleElement);
+  questionBox.appendChild(scoreElement);
+  questionBox.appendChild(closeButton);
+  questionBox.appendChild(questionHeader);
 }
